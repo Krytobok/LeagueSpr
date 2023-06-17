ce6c650a2a54343fbd85c7b5a0851a3374cd8ab4
Krytobok <102590165+Krytobok@users.noreply.github.com>
2023-06-17 16:23:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var myImage = document.querySelector('img');
+var images = ['img/form1.png', 'img/form2.png', 'img/form3.png'];
+var currentIndex = 0;
+
+myImage.onclick = function() {
+  currentIndex = (currentIndex + 1) % images.length;
+  myImage.setAttribute('src', images[currentIndex]);
+};
+
+alert("Автор не несет ответсвенность за моральный/психологический ущерб нанесенный данный оформлением.");
+
